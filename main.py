7a1ac50142791a638170ef36c30b60ed1159355d
artkis74 <kiselev_artem1991@mail.ru>
2022-10-15 13:48:17 +0500

    added iterator

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
+from Iterator import FlatIterator
+nested_list = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None],
+]
 
-# if __name__ == '__main__':
+# print(list(chain.from_iterable(nested_list)))
+if __name__ == '__main__':
+        for item in FlatIterator(nested_list):
+                print(item)
+        flat_list = [item for item in FlatIterator(nested_list)]
+        print(flat_list)
